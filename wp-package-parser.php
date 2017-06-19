963ce346a6bb5bd3383a9d8d8cb0cabcdbffd333
Max <tutv95@gmail.com>
2017-06-19 11:20:30 +0700

    Updated comment

--- wp-package-parser.php
+++ wp-package-parser.php
@@ -1,3 +1,13 @@
 <?php
 
+/**
+ * Title         : WP Package Parser
+ * Description   : Parsing WordPress plugin and theme metadata.
+ * Version       : 1.0.3
+ * Author        : Max
+ * Author URI    : https://github.com/tutv95
+ * License       : MIT
+ * Documentation : https://github.com/tutv95/wp-package-parser
+ */
+
 include_once 'src/class-max-wp-package.php';
